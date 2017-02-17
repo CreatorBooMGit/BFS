641b3822ea9938d5a7847d7e44ed6f320610df57
CreatorBooMGit <pautov.artem1998@gmail.com>
2017-02-18 01:05:07 +0200

    fix

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 //    pix->scaledToWidth(640);
     splash->setPixmap(*pix);
     splash->show();
-    splash->showMessage(QObject::tr("Курсовая работа"), Qt::AlignLeft | Qt::AlignTop, Qt::white);
+    splash->showMessage(QObject::tr("Курсовая работа \"Алгоритм поискав ширину\""), Qt::AlignLeft | Qt::AlignTop, Qt::white);
     splash->showMessage(QObject::tr("Загрузка..."), Qt::AlignLeft | Qt::AlignBottom, Qt::white);
     MainWindow w;
     splash->showMessage(QObject::tr("Запуск"), Qt::AlignLeft | Qt::AlignBottom, Qt::yellow);
